6cd0dca33e639f7ab989350947c1b3784a7851e1
Amaka04 <corneltina04@gmail.com>
2023-07-13 13:20:36 -0700

    printf c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints a given string
+ * main - printa a given string
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	puts("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
