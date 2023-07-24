ae603cfe3d7bf4641731a999b06463094d237c73
Amaka04 <corneltina04@gmail.com>
2023-07-24 04:26:45 -0700

    5-rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - function that prints string
+ * in reversed mode
+ *
+ * @s: the string input
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int length, i;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
