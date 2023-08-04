9e47afce9f6725f6194893630eb350ee8bad3c12
Amaka04 <corneltina04@gmail.com>
2023-08-04 15:16:06 -0700

    0x0A

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char c);
+int main(int argc _attribute_((unused)), char *argv[]);
+int main(int argc, char **argv _attribute_ ((unused)));
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char **argv);
 
 
 #endif
