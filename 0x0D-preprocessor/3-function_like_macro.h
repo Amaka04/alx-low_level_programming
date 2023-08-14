3bda49dcaa98f634e0b2803c253cdf7fa947030a
Amaka04 <corneltina04@gmail.com>
2023-08-14 02:40:02 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * File: 3-function_like_macro
+ * @x: parameter
+ * Description: header filewhich computes abs.. value
+ */
+#define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
+
+
+#endif
