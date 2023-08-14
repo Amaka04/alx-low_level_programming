5efa3b3af90a5a31d2ef0870e9c220b91deb5c64
Amaka04 <corneltina04@gmail.com>
2023-08-14 03:15:19 -0700

    struct_dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a struct that creates a dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: dog struct project
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
