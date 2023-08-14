1c8da3954dc2c13745207aa1152e27242586b04e
Amaka04 <corneltina04@gmail.com>
2023-08-14 02:46:45 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * File: 4-sum
+ *
+ * @x: first integer input
+ * @y: seecond integer input
+ * Description: sum up x and y
+ */
+#define SUM(x, y) (x + y)
+
+#endif
