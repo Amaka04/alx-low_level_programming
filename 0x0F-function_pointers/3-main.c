05a4f89e7f9110d2d5057045172a72029cbd9ec3
Amaka04 <corneltina04@gmail.com>
2023-08-16 08:55:58 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	/* set cmd argument input */
 	num1 = atoi(argv[1]); /* first number input */
 	op = (argv[2]); /* operator input */
@@ -26,26 +27,18 @@ int main(int argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(98);
-	}
-	/* set cmd arg input */
-	num1 = atoi(argv[1]); /* first number input */
-	op = (argv[2]); /* operator input */
-	num2 = atoi(argv[3]); /* second number input */
-
-	/* if opeator not in operator list */
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
 		exit(99);
 	}
+
 	/* if user tries to divide or perform modulus by 0 */
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	/* print results of operation */
 	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
