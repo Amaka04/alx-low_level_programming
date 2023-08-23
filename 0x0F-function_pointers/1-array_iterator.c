4a909039fe5072072a27ef3d3223dc4a372d9d2f
Amaka04 <corneltina04@gmail.com>
2023-08-23 14:34:01 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
+
 /**
  * array_iterator - iterates via an array
  * @array: target array
  * @size: size of the array
  * @action: function pointer to the array
+ *
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
