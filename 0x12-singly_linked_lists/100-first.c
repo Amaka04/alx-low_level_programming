caccaa7cc13055b6bf674dffd436b6700429cf7c
Amaka04 <corneltina04@gmail.com>
2023-08-24 14:57:48 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void ComeFirstbyAmaka(void)__attribute__((constructor));
+/**
+ * ComeFirstbyAmaka - function that runs before the main function
+ */
+void ComeFirstbyAmaka(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
