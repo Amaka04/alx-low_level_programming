f29df1fe27d48a12ea329a9a7b83af0d4c668d73
Amaka04 <corneltina04@gmail.com>
2023-08-24 04:39:27 -0700

    correction

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,6 +10,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_digit(char *s);
 void error(void);
 int _strlen(char *s);
-int main(int argc, char *argv[]);
 
 #endif
